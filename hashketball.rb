1ad2fc3a5ecc224fca267a0390ff9dd7d9ad1d48
David Lee <davidmlee4@gmail.com>
2020-01-27 21:48:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -150,6 +150,9 @@ def player_numbers(team_name)
 
   game_hash.each do |place,team|
     if team == team_name 
+      team.each do |attribute, data|
+        if attribute == :players #you must do this if statement if there is a hash but only one key of the hash leads to an array/hash
+          
 
   end
 end
