6f5889324d4913a82ff57be3802e0fee9be0af86
David Lee <davidmlee4@gmail.com>
2020-01-27 22:12:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -169,7 +169,8 @@ def player_stats(sought_player_name) #this is going to return player as a key an
       if data == :players #so again we must do this if statement because this hash containts three keys with the last key leading to a AoH
         data.each do |player|
           if player[:player_name] == sought_player_name
-            
+            binding.pry
+            result[sought_player_name] = player[:player_name]
           end
         end
       end
