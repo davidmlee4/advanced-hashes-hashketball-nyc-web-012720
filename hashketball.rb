d0f4ac87add69a3ab76adb7962429af3c01dc0d3
David Lee <davidmlee4@gmail.com>
2020-01-27 21:44:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -152,7 +152,7 @@ def player_numbers(team_name)
     team.each do |attribute, data|
       if attribute == :players
         data.each do |player|
-
+          player[:number]
         end
       end
     end
